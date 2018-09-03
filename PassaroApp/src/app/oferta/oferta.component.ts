ebde6e5cc1c078810e65dd9ef1c6a2b77c77a3e9
unknown <jhonatanqsilva@gmail.com>
2018-09-03 09:15:40 -0300

    Alteracoes em Oferta componente

--- PassaroApp/src/app/oferta/oferta.component.ts
+++ PassaroApp/src/app/oferta/oferta.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { OfertasService } from '../ofertas.service';
+import { Oferta } from '../shared/oferta.model';
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [OfertasService]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor() { }
+  public Oferta : Oferta
+  constructor(private route : ActivatedRoute, private ofertaService : OfertasService) {
+
+   }
 
   ngOnInit() {
+
+    let id  = this.route.snapshot.params['id']
+    this.ofertaService.getOfertaPorId(id)
+    .then((oferta:Oferta)=>this.Oferta = oferta)
   }
 
 }
--- PassaroApp/src/app/ofertas.service.ts
+++ PassaroApp/src/app/ofertas.service.ts
@@ -1,25 +1,29 @@
 import { Oferta } from "./shared/oferta.model";
 import { Http } from "@angular/http";
 import { Injectable } from "@angular/core";
+import { URL_API } from "./app.api";
 
 @Injectable()
 export class OfertasService{
 
-constructor(private http:Http)
-{
+constructor(private http:Http){}
 
-}
     public getOfertas() : Promise<Oferta[]>{
         
-       return this.http.get('http://localhost:3000/ofertas?destaque=true')
+       return this.http.get(`${URL_API}?destaque=true`)
         .toPromise()
         .then((resposta:any)=>resposta.json())
     } 
 
     public getOfertasPorCategoria(filtro:string) : Promise<Oferta[]>{
 
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${filtro}`)
+        return this.http.get(`${URL_API}?categoria=${filtro}`)
         .toPromise()
         .then((resposta:any)=>resposta.json())
+    }
+    public getOfertaPorId(id:string) : Promise<Oferta>{
+        return this.http.get(`${URL_API}?id=${id}`)
+        .toPromise()
+        .then((resposta:any)=>resposta.json()[0])
     }
 }
